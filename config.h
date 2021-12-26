f9da97997986f7f1c9d6c29cb69739c7127ff4ab
Shaw Terwilliger <sterwill@tinfig.com>
2021-12-26 14:02:52 -0500

    Added NWS settings

--- config.h
+++ config.h
@@ -1,2 +1,17 @@
 #define WIFI_SSID       "Your network here"
 #define WIFI_PASSPHRASE "Your passphrase here"
+
+/*
+ * Pufflux gets its forecast data from the USA's National Weather 
+ * Service web API.  Replace the values below with your local 
+ * weather office's name, and the X and Y coordinates of your
+ * local grid cell.  All values should be strings.
+ * 
+ * For more information, see:
+ * 
+ * https://www.weather.gov/documentation/services-web-api
+ * 
+ */
+#define NWS_OFFICE "RAH"
+#define NWS_GRID_X "62"
+#define NWS_GRID_Y "62"
